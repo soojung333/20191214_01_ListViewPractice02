254aa33994170c4cd00b447103e6624f1fb84b46
soojung <blessme333@gmail.com>
2019-12-14 11:04:04 +0900

    Initial commit

---
+++ app/src/main/java/com/soojung/a20191214_01_listviewpractice02/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.soojung.a20191214_01_listviewpractice02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
