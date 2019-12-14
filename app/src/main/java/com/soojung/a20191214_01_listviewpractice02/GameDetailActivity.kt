75ac336d429c71edec497e61387742bdcd68b457
soojung <blessme333@gmail.com>
2019-12-14 12:16:51 +0900

    게임 상세정보 화면 초안

---
+++ app/src/main/java/com/soojung/a20191214_01_listviewpractice02/GameDetailActivity.kt
@@ -0,0 +1,28 @@
+package com.soojung.a20191214_01_listviewpractice02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_detail)
+        setupEvents()
+        setValues()
+
+
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+
+}
