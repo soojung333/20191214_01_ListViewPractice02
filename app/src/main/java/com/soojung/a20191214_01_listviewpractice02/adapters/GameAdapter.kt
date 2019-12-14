6d7b1ffce93bd0f0464f50709d506dc3bcb5b68b
soojung <blessme333@gmail.com>
2019-12-14 12:05:07 +0900

    실제 위치에 맞는 데이터 출력

--- app/src/main/java/com/soojung/a20191214_01_listviewpractice02/adapters/GameAdapter.kt
+++ app/src/main/java/com/soojung/a20191214_01_listviewpractice02/adapters/GameAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.soojung.a20191214_01_listviewpractice02.R
 import com.soojung.a20191214_01_listviewpractice02.datas.GameData
 
@@ -21,6 +22,17 @@ class GameAdapter(context:Context, resId:Int, list:ArrayList<GameData>)
             tempRow = inf.inflate(R.layout.game_list_item, null)
         }
         val row = tempRow!!
+
+
+        val data = mList.get(position)
+
+        val gameTitleTxt = row.findViewById<TextView>(R.id.gameTitleTxt)
+        val gameCompanyTxt = row.findViewById<TextView>(R.id.gameCompanyTxt)
+
+        gameTitleTxt.text = data.title
+        gameCompanyTxt.text = data.companyName
+
+
 
         return row
 
