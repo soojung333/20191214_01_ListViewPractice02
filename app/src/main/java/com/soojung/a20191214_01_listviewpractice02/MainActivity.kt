889872165593cedbf048c93273ec7f35cec7fedf
soojung <blessme333@gmail.com>
2019-12-14 11:39:27 +0900

    게임 목록 추가기능 작성

--- app/src/main/java/com/soojung/a20191214_01_listviewpractice02/MainActivity.kt
+++ app/src/main/java/com/soojung/a20191214_01_listviewpractice02/MainActivity.kt
@@ -2,8 +2,14 @@ package com.soojung.a20191214_01_listviewpractice02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soojung.a20191214_01_listviewpractice02.datas.GameData
 
 class MainActivity : BaseActivity() {
+
+
+    val gameList = ArrayList<GameData>()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        addGames()
     }
+
+
+    fun addGames() {
+        gameList.add(GameData("아스팔트 8 : 에어본","Gameloft"))
+        gameList.add(GameData("MineCraft - Pocket Edition","Mojang"))
+        gameList.add(GameData("아스팔트 7 : 히트","Gameloft"))
+        gameList.add(GameData("팔라독(Paladog)","FazeCat"))
+        gameList.add(GameData("Plants Vs Zombies","EA Swiss Sarl"))
+        gameList.add(GameData("스왐피 (Swampy)","Disney"))
+
+    }
+
 
 
 }
